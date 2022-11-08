f6d1b8d9f6dfee550acf77edae1e95793dbf33e7
Roman Krompets <krompets@gmail.com>
2022-11-08 22:46:07 +0200

    Update up_sui.sh

--- up_sui.sh
+++ up_sui.sh
@@ -9,17 +9,22 @@
 
 source /root/.bash_profile
 
+CUREENT_VER=`wget -qO- https://api.github.com/repos/MystenLabs/sui/releases/latest | jq -r ".tag_name"`
+OLD_VER=`cat /root/.sui/version`
+
 main () {
   cd /root/sui
-  git stash
 
-  if [[ "$(git pull)" =~ "Already up to date." ]]; then
+  if [[ "$CUREENT_VER" == "$OLD_VER" ]]; then
     echo "No updates"
   else
     systemctl stop suid
     rm -rf /root/.sui/db
     wget -qO /root/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
     cd /root/sui
+    git stash
+    git fetch upstream
+    git checkout -B devnet --track upstream/devnet
     $(which cargo) build --release
     mv /root/sui/target/release/sui /usr/bin/
     mv /root/sui/target/release/sui-node /usr/bin/
@@ -28,6 +33,7 @@ main () {
     echo =================================== DONE!!! ====================================
     sui -V
     systemctl restart suid
+    echo "$CUREENT_VER" > /root/.sui/version
   fi
 }
 
